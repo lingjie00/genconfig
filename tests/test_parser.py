106b5076d113926520dd9014e2f666353a6d7a62
Lingjie Wang <wang.l.79@pg.com>
2022-09-29 22:47:29 +0800

    Updated unit testing for the modules

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,8 @@
 """Test the parser."""
-import unittest
-import tempfile  # create temp config files
-from contextlib import contextmanager
 import os
+import tempfile  # create temp config files
+import unittest
+from contextlib import contextmanager
 from typing import Tuple
 
 from configen.base_parser import Parser
@@ -19,39 +19,17 @@ class TestParser(unittest.TestCase):
     config_truth = {
         "name": "config-01",
         "training": True,
-        "parameters": {
-            "num_nodes": 200,
-            "num_samples": 100,
-            "max_time": 40
-        },
+        "parameters": {"num_nodes": 200, "num_samples": 100, "max_time": 40},
         "pipeline": [
-            {
-                "name": "extraction",
-                "function": "etl.extraction"
-            },
-            {
-                "name": "training",
-                "function": "model.training"
-            },
-            {
-                "name": "evaluation",
-                "function": "model.evaluation"
-            },
-            {
-                "name": "deployment",
-                "function": "cloud.deploy"
-            }
+            {"name": "extraction", "function": "etl.extraction"},
+            {"name": "training", "function": "model.training"},
+            {"name": "evaluation", "function": "model.evaluation"},
+            {"name": "deployment", "function": "cloud.deploy"},
         ],
         "function": {
-            "function1": {
-                "name": "transform",
-                "param": "col1"
-            },
-            "function2": {
-                "name": "load",
-                "param": "col2"
-            }
-        }
+            "function1": {"name": "transform", "param": "col1"},
+            "function2": {"name": "load", "param": "col2"},
+        },
     }
     new_config = {"name": "config-01"}
 
@@ -61,11 +39,11 @@ class TestParser(unittest.TestCase):
     dir_path = os.path.abspath(dir_path)
     config_path = {
         "json": os.path.join(dir_path, "sample.json"),
-        "yml": os.path.join(dir_path, "sample.yml")
+        "yml": os.path.join(dir_path, "sample.yml"),
     }
     config_folder = {
         "json": os.path.join(dir_path, "config-json/"),
-        "yml": os.path.join(dir_path, "config-yml/")
+        "yml": os.path.join(dir_path, "config-yml/"),
     }
     config_dict_raw = {}
 
@@ -90,9 +68,7 @@ class TestParser(unittest.TestCase):
     def test_write_tempfile(self):
         """Function should write a config to a specific filename."""
         config = {"name": "config1"}
-        with self.write_tempfile(
-                filename="config.json",
-                config=config) as filepath:
+        with self.write_tempfile(filename="config.json", config=config) as filepath:
             with open(filepath) as file:
                 self.assertEqual(file.read(), str(config), file.read())
 
@@ -111,7 +87,9 @@ class TestParser(unittest.TestCase):
             # given a extension that is not the parser extension, parser will
             # still append the extension
             self.assertEqual(
-                f"config.tmp.{ext}", parser._append_extension(f"config.tmp.{ext}"))
+                f"config.tmp.{ext}", parser._append_extension(
+                    f"config.tmp.{ext}")
+            )
             # raises assertion error when give non string input
             self.assertRaises(AssertionError, parser._append_extension, 123)
 
@@ -124,8 +102,8 @@ class TestParser(unittest.TestCase):
             # write a sample config
             sample_config = self.config_dict_raw[ext]
             with self.write_tempfile(
-                    filename="config." + ext,
-                    config=sample_config) as filename:
+                filename="config." + ext, config=sample_config
+            ) as filename:
                 # load the config
                 config_loaded = parser._load_method(filename)
                 # yaml will load the config as ordered dictionary
@@ -140,9 +118,7 @@ class TestParser(unittest.TestCase):
             ext = parser.extension
             parser = parser(self.config_truth)
             # write
-            with self.write_tempfile(
-                    filename="config." + ext,
-                    config="") as filename:
+            with self.write_tempfile(filename="config." + ext, config="") as filename:
                 # write the config
                 parser._write_method(filename)
                 # rely on the implemented load method
@@ -154,21 +130,24 @@ class TestParser(unittest.TestCase):
         """Function should be able to load single config, a folder of configs
         and a dictionary containing the config itself."""
         for parser in self.parsers:
+            # dictionary containing the config
             parser = parser()
             ext = parser.extension
+            loaded_config = parser.load(self.config_truth, replace=True).config
+            msg = f"loading {parser} with dictionary containing config"
+            self.assertEqual(loaded_config, self.config_truth, msg)
+
             # single config
             config_path = self.config_path[ext]
-            loaded_config = parser.load(config_path).config
-            self.assertEqual(loaded_config, self.config_truth, parser)
+            loaded_config = parser.load(config_path, replace=True).config
+            msg = f"loading {parser} with single config"
+            self.assertEqual(loaded_config, self.config_truth, msg)
 
             # folder of config
             config_folder = self.config_folder[ext]
-            loaded_config = parser.load(config_folder).config
-            self.assertEqual(loaded_config, self.config_truth, parser)
-
-            # dictionary containing the config
-            loaded_config = parser.load(self.config_truth).config
-            self.assertEqual(loaded_config, self.config_truth, parser)
+            loaded_config = parser.load(config_folder, replace=True).config
+            msg = f"loading {parser} with folder of config"
+            self.assertEqual(loaded_config, self.config_truth, msg)
 
     def test_write(self):
         """Function should be able to write stored config to file, or a new
@@ -177,17 +156,13 @@ class TestParser(unittest.TestCase):
             parser = parser(self.config_truth)
             ext = parser.extension
             # write stored config
-            with self.write_tempfile(
-                    filename="config." + ext,
-                    config="") as filename:
+            with self.write_tempfile(filename="config." + ext, config="") as filename:
                 parser.write(filename)
                 loaded_config = parser._load_method(filename)
                 self.assertEqual(loaded_config, self.config_truth, parser)
 
             # write new config
-            with self.write_tempfile(
-                    filename="config." + ext,
-                    config="") as filename:
+            with self.write_tempfile(filename="config." + ext, config="") as filename:
                 parser.write(filename, self.new_config)
                 loaded_config = parser._load_method(filename)
                 self.assertEqual(loaded_config, self.new_config, parser)
@@ -196,15 +171,18 @@ class TestParser(unittest.TestCase):
         """Function return True if regex match, else False."""
         parser = Parser()
         ignored = ("pipeline.*",)
-        self.assertTrue(parser._search_match("pipeline.json", check_list=ignored))
+        self.assertTrue(parser._search_match(
+            "pipeline.json", check_list=ignored))
         self.assertFalse(parser._search_match("nihao.yml", check_list=ignored))
 
         ignored = ("^pipeline.*",)
-        self.assertTrue(parser._search_match("pipeline.json", check_list=ignored))
+        self.assertTrue(parser._search_match(
+            "pipeline.json", check_list=ignored))
         self.assertFalse(parser._search_match("nihao.yml", check_list=ignored))
 
         ignored = (".*json",)
-        self.assertTrue(parser._search_match("pipeline.json", check_list=ignored))
+        self.assertTrue(parser._search_match(
+            "pipeline.json", check_list=ignored))
         self.assertFalse(parser._search_match("nihao.yml", check_list=ignored))
 
     def test_ignore(self):
@@ -215,7 +193,8 @@ class TestParser(unittest.TestCase):
         for parser in self.parsers:
             parser = parser()
             ext = parser.extension
-            parser.load(config=self.config_folder[ext], ignored=("pipeline.*",))
+            parser.load(
+                config=self.config_folder[ext], ignored=("pipeline.*",))
             loaded_config = dict(parser.config)
             self.assertEqual(loaded_config, config_truth, parser)
 
@@ -233,24 +212,29 @@ class TestParser(unittest.TestCase):
                 other_ext = parser.extension
                 # save the stored config
                 with self.write_tempfile(
-                        filename="config." + other_ext,
-                        config="") as filename:
+                    filename="config." + other_ext, config=""
+                ) as filename:
                     parser.convert(filename=filename, parser=other_parser)
                     loaded_config = other_parser._load_method(filename)
-                    self.assertEqual(loaded_config, self.new_config, f"{parser}, {other_parser}")
+                    self.assertEqual(
+                        loaded_config, self.new_config, f"{parser}, {other_parser}"
+                    )
 
                 # save the config path
                 with self.write_tempfile(
-                        filename="config." + other_ext,
-                        config="") as filename:
+                    filename="config." + other_ext, config=""
+                ) as filename:
                     config_path = self.config_path[ext]
                     parser.convert(
-                            filename=filename, parser=other_parser, config_path=config_path)
+                        filename=filename, parser=other_parser, config_path=config_path
+                    )
                     loaded_config = other_parser._load_method(filename)
                     # yaml load config as ordered dict
                     # we convert it back to dict for comparison
                     loaded_config = dict(loaded_config)
-                    self.assertEqual(loaded_config, self.config_truth, f"{parser}, {other_parser}")
+                    self.assertEqual(
+                        loaded_config, self.config_truth, f"{parser}, {other_parser}"
+                    )
 
 
 if __name__ == "__main__":
